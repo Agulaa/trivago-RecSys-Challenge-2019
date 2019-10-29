5842f1286d696ea35d151ceccc225b1952e95989
Agulaa <agnieszkaxrusin@gmail.com>
2019-10-29 10:19:56 +0100

    split train data to train set and test set

---
+++ baseline.py
@@ -0,0 +1,7 @@
+from  SystemRekomendacyjny.challange_function.src.baseline_algorithm import rec_popular as rp
+
+data_path = '/home/agnieszka/NAUKA/Studia/SEM VII/SR/SystemRekomendacyjny/data/data/'
+rp.main(data_path)
+
+
+
