1e542222bb354cf411245a57506908f22e631b43
Agulaa <agnieszkaxrusin@gmail.com>
2019-10-29 11:14:25 +0100

    add challange functions

--- free_function.py
+++ free_function.py
@@ -1,6 +1,6 @@
-from  SystemRekomendacyjny.challange_function.src.score_submission import score_subm as ss
-from  SystemRekomendacyjny.challange_function.src.verify_submission import verify_subm as vs
-from SystemRekomendacyjny.challange_function.src.baseline_algorithm import rec_popular as rp
+from challange_function.src.score_submission import score_subm as ss
+from challange_function.src.verify_submission import verify_subm as vs
+from challange_function.src.baseline_algorithm import rec_popular as rp
 
 subm_csv = '../data/submission_popular.csv'
 gt_csv = '../data/groundTruth.csv'
@@ -20,5 +20,5 @@ def score():
 if __name__ == '__main__':
 
    # baseline()
-    verify()
-    #score()
+    #verify()
+    score()
